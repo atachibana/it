6dc32471a42077868535d4b9d281dfe5032c14d8
Akira Tachibana <atachibana@unofficialtokyo.com>
2022-08-07 23:30:43 +0900

    GA4 support

---
+++ js/gtag.js
@@ -0,0 +1,9 @@
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-03DQ7ZFSWJ"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-03DQ7ZFSWJ');
+</script>
